d2291fb5cb57369ddadccacb3bb927cadfa38182
Joy-karmaker <48444326+Joy-karmaker@users.noreply.github.com>
2020-06-06 17:48:17 +0600

    updates

--- app/src/main/java/com/example/pstubloodbank/UrlClass.java
+++ app/src/main/java/com/example/pstubloodbank/UrlClass.java
@@ -4,53 +4,57 @@ package com.example.pstubloodbank;
  * Created by JOY KARMAKER on 13-Jan-18.
  */
 public class UrlClass {
-    public static String addDonor="http://192.168.43.223/blood/donor.php";
+
+    //public static String url="http://192.168.1.5/blood/";
+    public static String url="https://joykarmakerfirebase.000webhostapp.com/";
+
+    public static String addDonor = url+"donor.php";
    // public static String addDonor="https://joykarmakerfirebase.000webhostapp.com/donor.php";
 
-    public static String bloodAmount="http://192.168.43.223/blood/availableblood.php";
+    public static String bloodAmount=url+"availableblood.php";
     //public static String bloodAmount="https://joykarmakerfirebase.000webhostapp.com/availableblood.php";
 
-    public static String centerDetails="http://192.168.43.223/blood/centers.php";
+    public static String centerDetails=url+"centers.php";
     //public static String centerDetails="https://joykarmakerfirebase.000webhostapp.com/centers.php";
 
-    public static String profileUpdater= "http://192.168.43.223/blood/profileupdate.php";
+    public static String profileUpdater= url+"profileupdate.php";
    // public static String profileUpdater= "https://joykarmakerfirebase.000webhostapp.com/profileupdate.php";
 
-    public static  String donorProfile= "http://192.168.43.223/blood/profileuser.php";
+    public static  String donorProfile= url+"profileuser.php";
     //public static  String donorProfile= "https://joykarmakerfirebase.000webhostapp.com/profileuser.php";
 
-    public static String bloodDonorList= "http://192.168.43.223/blood/donorlist.php";
+    public static String bloodDonorList= url+"donorlist.php";
     //public static String bloodDonorList= "https://joykarmakerfirebase.000webhostapp.com/donorlist.php";
 
-    public static String displayProfile= "http://192.168.43.223/blood/profiledisplay.php";
+    public static String displayProfile= url+"profiledisplay.php";
     //public static String displayProfile= "https://joykarmakerfirebase.000webhostapp.com/profiledisplay.php";
 
-   public static String bloodRequest = "http://192.168.43.223/blood/request.php";
+   public static String bloodRequest = url+"request.php";
    // public static String bloodRequest = "https://joykarmakerfirebase.000webhostapp.com/request.php";
 
-    public static String bloodRequestWall= "http://192.168.43.223/blood/show.php";
+    public static String bloodRequestWall= url+"show.php";
     //public static String bloodRequestWall= "https://joykarmakerfirebase.000webhostapp.com/show.php";
 
-    public static String requestDiscription= "http://192.168.43.223/blood/requestdiscription.php";
+    public static String requestDiscription= url+"requestdiscription.php";
     //public static String requestDiscription= "https://joykarmakerfirebase.000webhostapp.com/requestdiscription.php";
 
-    public static String setLocation="http://192.168.43.223/blood/Location.php";
+    public static String setLocation=url+"Location.php";
     //public static String setLocation="https://joykarmakerfirebase.000webhostapp.com/Location.php";
 
-    public static String getLocation= "http://192.168.43.223/blood/getLocation.php";
+    public static String getLocation= url+"getLocation.php";
     //public static String getLocation= "https://joykarmakerfirebase.000webhostapp.com/getLocation.php";
 
-    public static String login= "http://192.168.43.223/blood/login.php";
+    public static String login= url+"login.php";
     //public static String login= "https://joykarmakerfirebase.000webhostapp.com/login.php";
 
-    public static String registration = "http://192.168.43.223/blood/Uregistration.php";
+    public static String registration = url+"Uregistration.php";
     //public static String registration = "https://joykarmakerfirebase.000webhostapp.com/Uregistration.php";
 
-    public static String mailer= "http://192.168.43.223/blood/mailer.php";
+    public static String mailer= url+"mailer.php";
     //public static String mailer= "https://joykarmakerfirebase.000webhostapp.com/mailer.php";
 
-    public static String adminForm= "http://192.168.43.223/blood/userform.php";
-    public static String donateStatus= "http://192.168.43.223/blood/donatestatus.php";
-    public static String smsStatus= "http://192.168.43.223/blood/smsstatus.php";
-    public static String amountStatus= "http://192.168.43.223/blood/amountstatus.php";
+    public static String adminForm= url+"userform.php";
+    public static String donateStatus= url+"donatestatus.php";
+    public static String smsStatus= url+"smsstatus.php";
+    public static String amountStatus= url+"amountstatus.php";
 }
